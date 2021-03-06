92284627dc74787bc77866d3a6c9d6267bb04c39
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-06 21:36:04 +0100

    First versions

---
+++ Private/Save-Resource.ps1
@@ -0,0 +1,29 @@
+﻿function Save-Resource {
+    [cmdletBinding()]
+    param(
+        [uri[]] $ResourceLinks,
+        [string[]] $Target,
+        [ValidateSet('CSS', 'JS')][string] $Type,
+        [string] $PathToSave
+    )
+    $TempProgressPreference = $ProgressPreference
+    $ProgressPreference = 'SilentlyContinue'
+    $Output = for ($i = 0; $i -lt $ResourceLinks.Count; $i++) {
+        if ($Target) {
+            # If target given means we replace something
+            Write-Verbose "Downloading $($ResourceLinks[$i].OriginalString) to $($Target[$i])"
+            Invoke-WebRequest -Uri $ResourceLinks[$i] -OutFile $Target[$i] -Verbose:$false
+            #$Target[$i]
+        } else {
+            $Splitted = $ResourceLinks[$i].OriginalString -split '/'
+            $FileName = $Splitted[ - 1]
+            $FilePath = [IO.Path]::Combine($PathToSave, $Type, $FileName)
+            $FilePathScriptRoot = -Join ('"', '$PSScriptRoot\..\Resources\', "$Type", '\', $FileName, '"')
+            $FilePathScriptRoot
+
+            Invoke-WebRequest -Uri $ResourceLinks[$i] -OutFile $FilePath -Verbose:$false
+        }
+    }
+    $ProgressPreference = $TempProgressPreference
+    $Output
+}
---
+++ Public/Save-HTMLResource.ps1
@@ -0,0 +1,37 @@
+﻿function Save-HTMLResource {
+    [cmdletBinding()]
+    param(
+        [string[]] $Keys,
+        [System.Collections.IDictionary] $Configuration,
+        [string] $PathToSave
+    )
+    foreach ($SearchKey in $Keys) {
+        $FoundKeys = $Configuration.Features.Keys | Where-Object { $_ -like $SearchKey }
+        foreach ($Key in $FoundKeys) {
+            if ($($Configuration).Features.$Key.Internal -eq $true) {
+                # Do not update, as it's internal code
+                continue
+            }
+            foreach ($Place in @('Header', 'Footer', 'Body')) {
+                if ($($Configuration).Features.$Key.$Place.JsLink -and $($Configuration).Features.$Key.$Place.Js) {
+                    if ($($Configuration).Features.$Key.$Place.JSLinkOriginal) {
+                        $JSLink = $($Configuration).Features.$Key.$Place.JsLinkOriginal
+                    } else {
+                        $JSLink = $($Configuration).Features.$Key.$Place.JsLink
+                    }
+                    Save-Resource -PathToSave $PathToSave -ResourceLinks $JSLink -Type 'JS' -Target $($Configuration).Features.$Key.$Place.Js -Verbose
+
+                }
+                if ($($Configuration).Features.$Key.$Place.CssLink -and $($Configuration).Features.$Key.$Place.Css) {
+                    if ($($Configuration).Features.$Key.$Place.CssLinkOriginal) {
+                        $CSSLink = $($Configuration).Features.$Key.$Place.CssLinkOriginal
+                    } else {
+                        $CSSLink = $($Configuration).Features.$Key.$Place.CssLink
+                    }
+                    Save-Resource -PathToSave $PathToSave -ResourceLinks $CSSLink -Type 'CSS' -Target $($Configuration).Features.$Key.$Place.Css -Verbose
+                }
+            }
+        }
+    }
+}
+
