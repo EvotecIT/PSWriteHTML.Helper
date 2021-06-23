3e2b3be791e490948bd24b57b0249e60630b9fe3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-06-23 09:09:27 +0200

    Add ability to skip update

--- Public/Save-HTMLResource.ps1
+++ Public/Save-HTMLResource.ps1
@@ -12,23 +12,25 @@
                 # Do not update, as it's internal code
                 continue
             }
-            foreach ($Place in @('Header', 'Footer', 'Body')) {
-                if ($($Configuration).Features.$Key.$Place.JsLink -and $($Configuration).Features.$Key.$Place.Js) {
-                    if ($($Configuration).Features.$Key.$Place.JSLinkOriginal) {
-                        $JSLink = $($Configuration).Features.$Key.$Place.JsLinkOriginal
-                    } else {
-                        $JSLink = $($Configuration).Features.$Key.$Place.JsLink
-                    }
-                    Save-Resource -PathToSave $PathToSave -ResourceLinks $JSLink -Type 'JS' -Target $($Configuration).Features.$Key.$Place.Js -Verbose
+            if ($Configuration.Features.$Key.Update -ne $false) {
+                foreach ($Place in @('Header', 'Footer', 'Body')) {
+                    if ($($Configuration).Features.$Key.$Place.JsLink -and $($Configuration).Features.$Key.$Place.Js) {
+                        if ($($Configuration).Features.$Key.$Place.JSLinkOriginal) {
+                            $JSLink = $($Configuration).Features.$Key.$Place.JsLinkOriginal
+                        } else {
+                            $JSLink = $($Configuration).Features.$Key.$Place.JsLink
+                        }
+                        Save-Resource -PathToSave $PathToSave -ResourceLinks $JSLink -Type 'JS' -Target $($Configuration).Features.$Key.$Place.Js -Verbose
 
-                }
-                if ($($Configuration).Features.$Key.$Place.CssLink -and $($Configuration).Features.$Key.$Place.Css) {
-                    if ($($Configuration).Features.$Key.$Place.CssLinkOriginal) {
-                        $CSSLink = $($Configuration).Features.$Key.$Place.CssLinkOriginal
-                    } else {
-                        $CSSLink = $($Configuration).Features.$Key.$Place.CssLink
                     }
-                    Save-Resource -PathToSave $PathToSave -ResourceLinks $CSSLink -Type 'CSS' -Target $($Configuration).Features.$Key.$Place.Css -Verbose
+                    if ($($Configuration).Features.$Key.$Place.CssLink -and $($Configuration).Features.$Key.$Place.Css) {
+                        if ($($Configuration).Features.$Key.$Place.CssLinkOriginal) {
+                            $CSSLink = $($Configuration).Features.$Key.$Place.CssLinkOriginal
+                        } else {
+                            $CSSLink = $($Configuration).Features.$Key.$Place.CssLink
+                        }
+                        Save-Resource -PathToSave $PathToSave -ResourceLinks $CSSLink -Type 'CSS' -Target $($Configuration).Features.$Key.$Place.Css -Verbose
+                    }
                 }
             }
         }
